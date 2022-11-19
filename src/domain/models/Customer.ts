e594b83e9641cf551164c027a2d2cacaaf1196e9
João Pedro <joao.pedroasz00@gmail.com>
2022-11-19 12:10:08 -0300

    feat: ensure Customer returns provided code

--- src/domain/models/Customer.ts
+++ src/domain/models/Customer.ts
@@ -1,17 +1,27 @@
+import { CustomerCode } from './CustomerCode'
+
 export type CustomerProps = {
-  id?: string
+  id: string
   document: string
   name: string
 }
 
 export class Customer {
-  private readonly id?: string
+  private readonly id: CustomerCode
   private readonly document: string
   private readonly name: string
 
-  constructor ({ id, document, name }: CustomerProps) {
-    this.id = id
+  constructor ({
+    id,
+    document,
+    name
+  }: CustomerProps) {
+    this.id = new CustomerCode({ value: id })
     this.document = document
     this.name = name
+  }
+
+  public getId (): string {
+    return this.id.getCode()
   }
 }

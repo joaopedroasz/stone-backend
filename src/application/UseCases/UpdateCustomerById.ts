c3563830342678ff79b77a686fc319b928068176
João Pedro <joao.pedroasz00@gmail.com>
2022-11-20 12:34:17 -0300

    feat: ensure UpdateCustomerById uses existent customer when data not provided

--- src/application/UseCases/UpdateCustomerById.ts
+++ src/application/UseCases/UpdateCustomerById.ts
@@ -1,14 +1,22 @@
-import { LoadCustomerByIdRepository } from '@/domain/repositories/Customer'
+import { Customer } from '@/domain/models'
+import { LoadCustomerByIdRepository, UpdateCustomerByIdRepository } from '@/domain/repositories/Customer'
 import { UpdateCustomerById, UpdateCustomerByIdInputDTO, UpdateCustomerByIdOutputDTO } from '../contracts'
 
 export class UpdateCustomerByIdUseCase implements UpdateCustomerById {
   constructor (
-    private readonly loadCustomerByIdRepository: LoadCustomerByIdRepository
+    private readonly loadCustomerByIdRepository: LoadCustomerByIdRepository,
+    private readonly updateCustomerByIdRepository: UpdateCustomerByIdRepository
   ) {}
 
   public async execute (input: UpdateCustomerByIdInputDTO): Promise<UpdateCustomerByIdOutputDTO> {
-    const { id } = input
-    await this.loadCustomerByIdRepository.load(id)
+    const { id, newCustomer } = input
+    const existentCustomer = await this.loadCustomerByIdRepository.load(id)
+    const customerToUpdate = new Customer({
+      id,
+      document: newCustomer.document ?? existentCustomer.getDocument(),
+      name: newCustomer.name ?? existentCustomer.getName()
+    })
+    await this.updateCustomerByIdRepository.update(customerToUpdate)
 
     return {
       id: 'any_id',

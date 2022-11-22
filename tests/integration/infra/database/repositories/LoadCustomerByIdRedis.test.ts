39289559217343e20c4a878baf02b9a8b2dc6e91
João Pedro <joao.pedroasz00@gmail.com>
2022-11-22 17:42:56 -0300

    test: ensure LoadCustomerByIdRepo returns undefined if no customer found

---
+++ tests/integration/infra/database/repositories/LoadCustomerByIdRedis.test.ts
@@ -0,0 +1,54 @@
+import { Customer } from '@/domain/models'
+import { LoadCustomerByIdRepository } from '@/domain/repositories/Customer'
+import { DatabaseConnection, LoadCustomerByIdRedisRepository } from '@/infra/database'
+import { connection } from '@/tests/utils'
+
+const makeCustomer = (): Customer => new Customer({
+  id: 'any_id',
+  document: 100,
+  name: 'any_name'
+})
+
+type SutType = {
+  sut: LoadCustomerByIdRepository
+  connection: DatabaseConnection
+}
+
+const makeSut = (): SutType => ({
+  sut: new LoadCustomerByIdRedisRepository(connection),
+  connection
+})
+
+describe('LoadCustomerByIdRedisRepository', () => {
+  beforeEach(async () => {
+    await connection.connect()
+  })
+
+  afterEach(async () => {
+    await connection.disconnect()
+  })
+
+  it('should load a customer by id', async () => {
+    const { sut, connection } = makeSut()
+    const customer = makeCustomer()
+    const key = `customer:${customer.getId()}`
+    const stringifiedCustomer = JSON.stringify({
+      id: customer.getId(),
+      document: customer.getDocument(),
+      name: customer.getName()
+    })
+    await connection.set(key, stringifiedCustomer)
+
+    const loadedCustomer = await sut.load(customer.getId())
+
+    expect(loadedCustomer).toEqual(customer)
+  })
+
+  it('should return undefined if customer does not exist', async () => {
+    const { sut } = makeSut()
+
+    const loadedCustomer = await sut.load('any_invalid_id')
+
+    expect(loadedCustomer).toBeUndefined()
+  })
+})

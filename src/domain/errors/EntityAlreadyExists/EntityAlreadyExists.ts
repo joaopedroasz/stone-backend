812ad779d8a5afad7730e6ab36bf54e0937d95db
João Pedro <joao.pedroasz00@gmail.com>
2022-11-23 14:28:04 -0300

    feat: add errors codes in custom errors

--- src/domain/errors/EntityAlreadyExists/EntityAlreadyExists.ts
+++ src/domain/errors/EntityAlreadyExists/EntityAlreadyExists.ts
@@ -4,6 +4,8 @@ export type EntityAlreadyExistsProps = {
 }
 
 export class EntityAlreadyExistsError extends Error {
+  protected readonly code: number = 409
+
   constructor (entityName: string, props: EntityAlreadyExistsProps) {
     super(`Entity ${entityName} with ${props.targetProperty} ${props.targetValue} already exists`)
     this.name = 'EntityAlreadyExistsError'
--- src/domain/errors/EntityNotFound/EntityNotFound.ts
+++ src/domain/errors/EntityNotFound/EntityNotFound.ts
@@ -4,6 +4,8 @@ export type EntityNotFoundProps = {
 }
 
 export class EntityNotFoundError extends Error {
+  protected readonly code: number = 404
+
   constructor (entityName: string, props: EntityNotFoundProps) {
     super(`Entity ${entityName} not found with ${props.targetProperty} ${props.targetValue}`)
     this.name = 'EntityNotFoundError'

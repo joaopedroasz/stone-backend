4cda0edc890efdc3f0b6dcfe1a8af67d2ff7b2d3
João Pedro <joao.pedroasz00@gmail.com>
2022-11-23 18:06:08 -0300

    feat: ensure LoadCustomerByHttpController calls use case correctly

--- src/infra/http/controllers/LoadCustomerByIdHttp.ts
+++ src/infra/http/controllers/LoadCustomerByIdHttp.ts
@@ -1,11 +1,19 @@
+import { LoadCustomerById } from '@/application/contracts'
 import { HttpResponse, LoadCustomerByIdHttp, LoadCustomerByIdHttpInputDTO, LoadCustomerByIdHttpOutputDTO } from '../contracts'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class LoadCustomerByIdHttpController implements LoadCustomerByIdHttp {
+  constructor (
+    private readonly loadCustomerById: LoadCustomerById
+  ) {}
+
   public async handle (request: LoadCustomerByIdHttpInputDTO): Promise<HttpResponse<LoadCustomerByIdHttpOutputDTO | Error>> {
     const error = this.validateRequest(request)
     if (error) return badRequest(error)
+
+    const { customerId } = request
+    await this.loadCustomerById.execute({ customerId })
 
     return {
       statusCode: 0,

b81803193d51efa7df5ee2654e8e8351d22d3399
João Pedro <joao.pedroasz00@gmail.com>
2022-11-23 15:09:22 -0300

    feat: ensure UpdateCustomerByIdHttpController calls use case correctly

--- src/infra/http/controllers/UpdateCustomerByIdHttp.ts
+++ src/infra/http/controllers/UpdateCustomerByIdHttp.ts
@@ -1,11 +1,27 @@
+import { UpdateCustomerById, UpdateCustomerByIdInputDTO } from '@/application/contracts'
 import { HttpResponse, UpdateCustomerByIdHttp, UpdateCustomerByIdHttpInputDTO, UpdateCustomerByIdHttpOutputDTO } from '../contracts'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class UpdateCustomerHttpByIdController implements UpdateCustomerByIdHttp {
+  constructor (
+    private readonly updateCustomerById: UpdateCustomerById
+  ) {}
+
   public async handle (request: UpdateCustomerByIdHttpInputDTO): Promise<HttpResponse<UpdateCustomerByIdHttpOutputDTO | Error>> {
     const errorInRequest = this.validateRequest(request)
     if (errorInRequest) return badRequest(errorInRequest)
+
+    const { existentCustomerId, newId, newName, newDocument } = request
+    const input: UpdateCustomerByIdInputDTO = {
+      id: existentCustomerId,
+      newCustomer: {
+        id: newId,
+        name: newName,
+        document: newDocument
+      }
+    }
+    await this.updateCustomerById.execute(input)
 
     return {
       statusCode: 0,

b9869bc053eb7b3f7a567f2d235f6df78e638c49
João Pedro <joao.pedroasz00@gmail.com>
2022-11-23 18:11:36 -0300

    feat: ensure LoadCustomerByHttpController returns serverError if use case throws

--- src/infra/http/controllers/LoadCustomerByIdHttp.ts
+++ src/infra/http/controllers/LoadCustomerByIdHttp.ts
@@ -1,7 +1,7 @@
 import { LoadCustomerById } from '@/application/contracts'
 import { HttpResponse, LoadCustomerByIdHttp, LoadCustomerByIdHttpInputDTO, LoadCustomerByIdHttpOutputDTO } from '../contracts'
 import { MissingParamError } from '../errors'
-import { badRequest, success } from '../helpers'
+import { badRequest, serverError, success, unknownError } from '../helpers'
 
 export class LoadCustomerByIdHttpController implements LoadCustomerByIdHttp {
   constructor (
@@ -9,13 +9,20 @@ export class LoadCustomerByIdHttpController implements LoadCustomerByIdHttp {
   ) {}
 
   public async handle (request: LoadCustomerByIdHttpInputDTO): Promise<HttpResponse<LoadCustomerByIdHttpOutputDTO | Error>> {
-    const error = this.validateRequest(request)
-    if (error) return badRequest(error)
+    try {
+      const error = this.validateRequest(request)
+      if (error) return badRequest(error)
 
-    const { customerId } = request
-    const loadedCustomer = await this.loadCustomerById.execute({ customerId })
+      const { customerId } = request
+      const loadedCustomer = await this.loadCustomerById.execute({ customerId })
 
-    return success<LoadCustomerByIdHttpOutputDTO>(loadedCustomer)
+      return success<LoadCustomerByIdHttpOutputDTO>(loadedCustomer)
+    } catch (error) {
+      const isError = error instanceof Error
+      if (!isError) return unknownError(error)
+
+      return serverError(error)
+    }
   }
 
   private validateRequest (request: LoadCustomerByIdHttpInputDTO): MissingParamError | undefined {

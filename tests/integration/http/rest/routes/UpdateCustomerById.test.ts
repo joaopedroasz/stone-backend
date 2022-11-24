6b1fb69dc2df8d4029e2a88dfb50708cd40dccee
João Pedro <joao.pedroasz00@gmail.com>
2022-11-24 10:27:56 -0300

    test: ensure UpdateCustomerByIdRoute return 200 when update customer

---
+++ tests/integration/http/rest/routes/UpdateCustomerById.test.ts
@@ -0,0 +1,61 @@
+import request from 'supertest'
+
+import { ServerHttpRestExpressAdapter, UpdateCustomerByIdRoute } from '@/infra/http'
+import { connection } from '@/tests/utils'
+
+describe('UpdateCustomerByIdRoute', () => {
+  const server = new ServerHttpRestExpressAdapter()
+  new UpdateCustomerByIdRoute(server, connection)
+
+  beforeAll(async () => {
+    await connection.connect()
+  })
+
+  afterAll(async () => {
+    await connection.disconnect()
+  })
+
+  it('should return 200 is customer is updated', async () => {
+    const createdCustomer = {
+      id: 'existent_id',
+      name: 'any_name',
+      document: 12345678910
+    }
+    const key = `customer:${createdCustomer.id}`
+    await connection.set(key, JSON.stringify(createdCustomer))
+
+    const response = await request(server.express).put(`/customers/${createdCustomer.id}`).send({
+      newId: 'new_customer_id',
+      newName: 'new_name',
+      newDocument: 98765432101
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      id: 'new_customer_id',
+      name: 'new_name',
+      document: 98765432101
+    })
+  })
+
+  it('should return 200 when update only id', async () => {
+    const createdCustomer = {
+      id: 'existentId',
+      name: 'any_name',
+      document: 12345678910
+    }
+    const key = `customer:${createdCustomer.id}`
+    await connection.set(key, JSON.stringify(createdCustomer))
+
+    const response = await request(server.express).put(`/customers/${createdCustomer.id}`).send({
+      newId: 'new_customerId'
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      id: 'new_customerId',
+      name: 'any_name',
+      document: 12345678910
+    })
+  })
+})

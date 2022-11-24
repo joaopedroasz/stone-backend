97c35d880ec175970b5a426f7ecda5ad94658d6d
João Pedro <joao.pedroasz00@gmail.com>
2022-11-24 09:43:21 -0300

    feat: add new methods in database connection contract

--- src/infra/database/contracts/RedisConnection.ts
+++ src/infra/database/contracts/RedisConnection.ts
@@ -2,4 +2,6 @@ export interface DatabaseConnection {
   get: (key: string) => Promise<string | undefined>
   set: (key: string, value: string) => Promise<boolean>
   delete: (key: string) => Promise<boolean>
+  connect: () => Promise<void>
+  disconnect: () => Promise<void>
 }

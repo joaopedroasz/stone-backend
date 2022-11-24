a0b14d585198dc94947838533a888462d547dad9
João Pedro <joao.pedroasz00@gmail.com>
2022-11-24 10:10:27 -0300

    test: ensure LoadCustomerByIdRoute returns 502 if database connection fails

--- tests/integration/http/rest/routes/LoadCustomerById.test.ts
+++ tests/integration/http/rest/routes/LoadCustomerById.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 
 import { LoadCustomerByIdRoute, ServerHttpRestExpressAdapter } from '@/infra/http'
 import { connection } from '@/tests/utils'
+import { ConnectionNotEstablishedError } from '@/infra/database'
 
 describe('LoadCustomerByIdRoute', () => {
   const server = new ServerHttpRestExpressAdapter()
@@ -35,5 +36,23 @@ describe('LoadCustomerByIdRoute', () => {
 
     expect(response.status).toBe(404)
     expect(response.body).toEqual({ error: 'Entity customer not found with id invalid_id' })
+  })
+
+  it('should return 502 if redis connection throws ConnectionNotEstablishedError', async () => {
+    jest.spyOn(connection, 'get').mockRejectedValueOnce(new ConnectionNotEstablishedError())
+    const createdCustomer = {
+      id: 'valid_id',
+      name: 'any_name',
+      document: 12345678910
+    }
+    const key = `customer:${createdCustomer.id}`
+    await connection.set(key, JSON.stringify(createdCustomer))
+
+    const response = await request(server.express).get(`/customers/${createdCustomer.id}`)
+
+    expect(response.status).toBe(502)
+    expect(response.body).toEqual({
+      error: 'Connection not established'
+    })
   })
 })

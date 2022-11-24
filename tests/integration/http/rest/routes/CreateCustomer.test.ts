a3371894dee5a511e3bee586eaf41828abf82b8a
João Pedro <joao.pedroasz00@gmail.com>
2022-11-24 09:44:16 -0300

    test: ensure CreateCustomerRoute returns 502 if database throws

---
+++ tests/integration/http/rest/routes/CreateCustomer.test.ts
@@ -0,0 +1,61 @@
+import request from 'supertest'
+
+import { CreateCustomerRoute, ServerHttpRestExpressAdapter } from '@/infra/http'
+import { ConnectionNotEstablishedError } from '@/infra/database'
+import { connection } from '@/tests/utils'
+
+describe('CreateCustomerRoute', () => {
+  const server = new ServerHttpRestExpressAdapter()
+  new CreateCustomerRoute(server, connection)
+
+  beforeAll(async () => {
+    await connection.connect()
+  })
+
+  afterAll(async () => {
+    await connection.disconnect()
+  })
+
+  it('should return 400 if no name is provided', async () => {
+    const input = {
+      name: '',
+      document: 12345678910
+    }
+
+    const response = await request(server.express).post('/customers').send(input)
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({
+      error: 'Missing param: name'
+    })
+  })
+
+  it('should return 400 if no document is provided', async () => {
+    const input = {
+      name: 'any_name',
+      document: ''
+    }
+
+    const response = await request(server.express).post('/customers').send(input)
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({
+      error: 'Missing param: document'
+    })
+  })
+
+  it('should return 502 if redis connection throws ConnectionNotEstablishedError', async () => {
+    jest.spyOn(connection, 'set').mockRejectedValueOnce(new ConnectionNotEstablishedError())
+    const input = {
+      name: 'any_name',
+      document: 12345678910
+    }
+
+    const response = await request(server.express).post('/customers').send(input)
+
+    expect(response.status).toBe(502)
+    expect(response.body).toEqual({
+      error: 'Connection not established'
+    })
+  })
+})
